dbde678d2b49a9715c3f423867b23adc33b58011
LiangFaCai <liang2944782717@163.com>
2019-02-12 17:13:34 +0800

    ceshi

--- src/main/java/com/javen/controller/Test.java
+++ src/main/java/com/javen/controller/Test.java
@@ -1,5 +1,16 @@
 package com.javen.controller;
 
 public class Test {
+	
+	
+	private String message;
 
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+   
 }
