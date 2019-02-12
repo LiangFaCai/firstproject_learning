fc01ef888142be03a3168e13c8bc202ac54a13cc
LiangFaCai <liang2944782717@163.com>
2019-02-12 17:29:48 +0800

    更新时间

--- src/main/java/com/javen/controller/Test.java
+++ src/main/java/com/javen/controller/Test.java
@@ -1,9 +1,20 @@
 package com.javen.controller;
 
+import java.util.Date;
+
 public class Test {
 	
 	
 	private String message;
+	private Date updateTime;
+	
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 
 	public String getMessage() {
 		return message;
